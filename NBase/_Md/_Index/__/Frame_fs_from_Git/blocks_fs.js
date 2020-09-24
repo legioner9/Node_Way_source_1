a23233a9902452cb9ab9fc6fca3f469912117794
legioner9 <legioner9@inbox.ru>
2020-09-25 02:49:23 +0700

    <> Frame_fs_from_Git 1

---
+++ NBase/_Md/_Index/__/Frame_fs_from_Git/blocks_fs.js
@@ -0,0 +1,33 @@
+const _src = [ '_src', 0 ];
+
+const Common_this = [ 'Common_this', 'Common_this.md',
+    _src ];
+const Docs_this = [ 'Docs_this', 'Docs_this.md',
+    _src ];
+const www_this = [ 'www_this', 'www_this.md',
+    _src ];
+
+const Live_learn = [ 'Live_learn', 'Live_learn.md', 'ReadMe.md',
+    Common_this,
+    Docs_this,
+    www_this ];
+
+const Point_learn = [ 'Point_learn', 'Point_learn.md', 'ReadMe.md',
+    Common_this,
+    Docs_this,
+    www_this ];
+
+const Intro = [ 'Intro', 'Intro.md', 'ReadMe.md',
+    Common_this,
+    Docs_this,
+    www_this ];
+
+const Use_this = [ 'Use_this', 'Use_this.md',
+    [ 'First_steps', 'First_steps.md', _src ],
+    [ 'Hints', 'Hints.md', _src ] ];
+
+const Workout_this = [ 'Workout_this', 'Workout_this.md', _src ];
+
+
+
+
