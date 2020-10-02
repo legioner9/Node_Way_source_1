92227de517b15c7cff115fe0b13d757ceeefb56c
legioner9 <legioner9@inbox.ru>
2020-10-02 19:47:10 +0700

    <> Advanced_Bash-Scripting _Guide step_2

--- NBase/_Md/_Index/_Bash_{LG2}/Containers/Live_learn/Advanced_Bash-Scripting _Guide/Book_content_files/cloneFileToDir_in_2_3_level.js
+++ NBase/_Md/_Index/_Bash_{LG2}/Containers/Live_learn/Advanced_Bash-Scripting _Guide/Book_content_files/cloneFileToDir_in_2_3_level.js
@@ -1,6 +1,6 @@
 const cloneFileToDir_8 = require ( 'st_ini' ).cloneFileToDir_8;
 cloneFileToDir_8.mode = 'production';
-cloneFileToDir_8 ( __dirname , 'other.md');
+cloneFileToDir_8 ( __dirname , '_.Reference.md');
 
 
 
