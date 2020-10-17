495e58c18a4f2939d365f3519fdad499b34140d0
legioner9 <legioner9@inbox.ru>
2020-10-17 13:48:29 +0700

    <> STN_func_v_1_test_1 step_2

---
+++ Jobs/FuncStandarts/STN_func_v_1_test_1/Test/main_test.js
@@ -0,0 +1,20 @@
+const Ex = require ( 'st_ex1' );
+const Ini = require ( 'st_ini' );
+debugger
+const func_v_1_test_1 = require ( '../index' );
+
+// set behavior ModuleFunc in runtime
+// func_v_1_test_1.mode = { deb: false, log: false, logFs: false, stack: false};
+
+// subscription to reports on ModuleFunc
+// func_v_1_test_1.event.on ( 'start ModuleFunc', d => {} );
+// func_v_1_test_1.event.on ( 'point ModuleFunc', d => {} );
+// func_v_1_test_1.event.on ( 'end ModuleFunc', d => {} );
+
+// func_v_1_test_1 InitState reports
+// func_v_1_test_1.event.emit ('init ModuleFunc', d = [__dirname])
+
+// func_v_1_test_1 Case_1 reports
+// func_v_1_test_1.event.emit ('init case_1', true)
+
+debugger;
