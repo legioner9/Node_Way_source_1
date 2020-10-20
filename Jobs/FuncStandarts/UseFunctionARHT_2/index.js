f8c61d64470a1f7e8afb920facde7ebc2ec90936
legioner9 <legioner9@inbox.ru>
2020-10-20 10:00:45 +0700

    <> UseFunctionARHT_2 step_2

--- Jobs/FuncStandarts/UseFunctionARHT_2/index.js
+++ Jobs/FuncStandarts/UseFunctionARHT_2/index.js
@@ -1,11 +1,13 @@
 const Arht = require ( 'st_ini_arht' );
-const atOut_1 = Arht.atOut_1;
+const atOut_2 = Arht.atOut_2;
 debugger
 const fn = () => {
+    debugger
+    const ENVARHT = atOut_2.at ( fn );
     console.log ( 'RUN main tread' );
 };
-atOut_1.prop ( fn );
-atOut_1.out ( fn );
+atOut_2.prop ( fn );
+atOut_2.out ( fn );
 fn.mode.logFs = true;
 
 debugger
