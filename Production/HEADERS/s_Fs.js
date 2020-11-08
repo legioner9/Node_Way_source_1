0c491bd18be9f0539dc6e3d8faadbb2a8bf5ac72
legioner9 <legioner9@inbox.ru>
2020-11-08 16:59:51 +0700

    <> arht.before in map to all s_Fs + HEADERS.js

---
+++ Production/HEADERS/s_Fs.js
@@ -0,0 +1,6 @@
+const Path = require ( 'path' );
+const s_Fs = require ( 'st_ini_fs' );
+const { arht } = require ( 'st_ini_arht' );
+
+const arr_names = Object.getOwnPropertyNames ( s_Fs );
+arr_names.map ( item => arht.before ( s_Fs[item], module ) );
