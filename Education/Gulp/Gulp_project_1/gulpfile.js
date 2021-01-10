6c1a10461e2d760aacb97cc3e46307adaf5d1bf4
legioner9 <legioner9@inbox.ru>
2021-01-10 14:06:37 +0700

    <> init TSH Asinc_Programming

--- Education/Gulp/Gulp_project_1/gulpfile.js
+++ Education/Gulp/Gulp_project_1/gulpfile.js
@@ -1,29 +1,28 @@
 'use strict';
 
-const {src, dest, watch, series, parallel} = require('gulp');
-const del = require('del');
+const { src, dest, watch, series, parallel } = require ( 'gulp' );
+const del = require ( 'del' );
 
-const tN = (callback)=> {
+const tN = ( callback ) => {
     // что-то делаем
-    console.log('Привет, Gulp!');
-    callback();
-}
+    console.log ( 'Привет, Gulp!' );
+    callback ();
+};
 
-function yourTask_1() {
-    return src('app/assets/css/**.css')   // выбираем все css-файлы
+const copy_css = () => {
+    return src ( 'app/assets/css/**.css' )   // выбираем все css-файлы
         // что-то делаем
-        .pipe(dest('dist/assets/css/'))   // и выгружаем
-}
-function yourTask_2() {
-    return src('app/assets/css/**.css')   // выбираем все css-файлы
-        // что-то делаем
-        .pipe(dest('dist/assets/css/'))   // и выгружаем
-}
+        .pipe ( dest ( 'dist/assets/css/' ) );   // и выгружаем
+};
 
-function clean() {
-    return del('./dist/');
-}
-exports.clean = clean;
+const clean_css_than = () => {
+    return del ( './dist/assets/css/' ).then ( copy_css );
+};
+
+const clean_css = async () => {
+    return del ( './dist/assets/css/' )
+        // .then ( () =>{});
+};
 
 // src (sourse) - выбираем файлы для обработки;
 // dest (destination) - место назначения.
@@ -34,5 +33,6 @@ exports.clean = clean;
 // ['app/**/*.css', 'app/**/*.js'] - получаем сначала все css, потом все js в папке app;
 // ['app/**/*.*', '!app/**/*.js'] - все файлы в папке app, кроме js-файлов. (! - знак исключения).
 
-exports.parallel_series = series(clean() , series (yourTask_1,yourTask_2))
+exports.clco_css_ser = series ( clean_css,copy_css ); // well done but((
+exports.clco_css_par = parallel (  clean_css,copy_css ); // some with error
 
--- Education/Gulp/Gulp_project_gulp_pug_1/ReadMe.md
+++ Education/Gulp/Gulp_project_gulp_pug_1/ReadMe.md
@@ -1,4 +1,6 @@
-# [Getting Started](https://pugjs.org/api/getting-started.html)
+# [Getting Started (Pug)](https://pugjs.org/api/getting-started.html)
+
+# [Quick Start (Gulp)](https://gulpjs.com/docs/en/getting-started/quick-start)
 
 # [Современная сборка 2020 для frontend. Gulp4](https://habr.com/ru/post/484714/)
 
