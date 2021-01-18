1d62b66b3f36d964cf1a3bdb0ab36a064505ee7e
legioner9 <legioner9@inbox.ru>
2021-01-18 19:37:35 +0700

    <> init computerConstract Theory

--- NBase/_Md/_Index/_CompArhitecture/Theory/cloneFileToDir_in_num_2_3_level_src_1.js
+++ NBase/_Md/_Index/_CompArhitecture/Theory/cloneFileToDir_in_num_2_3_level_src_1.js
@@ -2,7 +2,7 @@ const cloneFileToDir_7_src_1 = require ( 'st_ini' ).cloneFileToDir_7_src_1;
 cloneFileToDir_7_src_1.mode = 'production';
 cloneFileToDir_7_src_1 ( __dirname, 'excf.Theory.md' );
 
-process.exit(0);
+
 
 
 
