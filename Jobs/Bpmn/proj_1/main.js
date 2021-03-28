8b46c4f98a9deaabacd71805f68bc15c74ea94f2
Legioner9 <legioner9@inbox.ru>
2021-03-28 10:53:18 +0700

    <> free graph pr_4.bpmn

--- Jobs/Bpmn/proj_1/main.js
+++ Jobs/Bpmn/proj_1/main.js
@@ -9,4 +9,6 @@ const o = Fs.readFileSync('./xml/1.xml')
 
 const obj_from_xml = pars('./xml/1.xml');
 const obj_from_bpmn = pars('./xml/Init_project_storm_bpmn_2.bpmn');
+const obj_3 = pars('./xml/pr_3.bpmn');
+const obj_4 = pars('./xml/pr_4.bpmn');
 debugger
