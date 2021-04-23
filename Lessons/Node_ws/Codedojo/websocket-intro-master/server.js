3713ab930dab29e1c8e2be84d328e8617c119277
Legioner9 <legioner9@inbox.ru>
2021-04-24 01:53:26 +0700

    <> manual NpmQuickServers and Codedojo/websocket-intro-master/server.js complete

--- Lessons/Node_ws/Codedojo/websocket-intro-master/server.js
+++ Lessons/Node_ws/Codedojo/websocket-intro-master/server.js
@@ -1,9 +1,19 @@
 const { _require } = require ( 'st_require' );
 const WS = _require ( 'ws' );
-debugger
+
 
 const wss = new WS.Server ( { port: 3000 } );
-
+debugger
 wss.on ( 'connection', ws => {
+    ws.on('message', message =>{
+
+        wss.clients.forEach(client =>{
+            if(client.readyState === WS.OPEN){
+                client.send(message);
+            }
+
+        })
+
+    })
     ws.send ( 'Connect is existing' );
 } );
---
+++ NBase/_Md/_Index/__/NpmQuickServers/ReadMe.md
@@ -0,0 +1,8 @@
+##  [html5.by](https://html5.by/blog/run-http-server-from-current-directory/)
+### http-server (Node.js)
+    npm install -g http-server # install dependency
+    http-server -p 8000 
+    
+### node-static (Node.js)
+    npm install -g node-static # install dependency 
+    static -p 8000
