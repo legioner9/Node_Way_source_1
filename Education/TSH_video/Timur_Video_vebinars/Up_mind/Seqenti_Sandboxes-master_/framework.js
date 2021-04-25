6824af92e75cc11a561477a6b7754bf573784aa5
Legioner9 <legioner9@inbox.ru>
2021-04-25 10:48:10 +0700

    <> init Seqenti_Sandboxes-master_

---
+++ Education/TSH_video/Timur_Video_vebinars/Up_mind/Seqenti_Sandboxes-master_/framework.js
@@ -0,0 +1,8 @@
+const { _require } = require ( 'st_require' );
+const arht = new (_require ( '_aop' ).archetypes.class.arht);
+const _node = _require ( '_node' );
+const _utest = _require ( '_utest_ini' );
+
+const PRSING_TIMEOUT = 1000;
+const EXECUTION_TIMEOUT = 5000;
+
