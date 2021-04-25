c1f053e8740de73bf33d71f6b940f97ef9c0753a
Legioner9 <legioner9@inbox.ru>
2021-04-25 14:42:21 +0700

    <> init Seqenti_Sandboxes-master_

--- Education/TSH_video/Timur_Video_vebinars/Up_mind/Seqenti_Sandboxes-master_/framework.js
+++ Education/TSH_video/Timur_Video_vebinars/Up_mind/Seqenti_Sandboxes-master_/framework.js
@@ -1,8 +1,15 @@
 const { _require } = require ( 'st_require' );
-const arht = new (_require ( '_aop' ).archetypes.class.arht);
+const arht = new ( _require ( '_aop' ).archetypes.class.arht );
 const _node = _require ( '_node' );
-const _utest = _require ( '_utest_ini' );
+const _utest = _require ( '_utest' );
 
 const PRSING_TIMEOUT = 1000;
 const EXECUTION_TIMEOUT = 5000;
+
+const { wm } = _node;
+
+const __node = Object.assign ( {}, _node );
+delete __node.fs
+
+
 
