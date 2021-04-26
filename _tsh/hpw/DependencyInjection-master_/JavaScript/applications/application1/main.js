3b62e6215fad8a41d83ef12c648fd011e4a1a95d
Legioner9 <legioner9@inbox.ru>
2021-04-26 13:52:52 +0700

    <> my ./sandboxed-fs

--- _tsh/hpw/DependencyInjection-master_/JavaScript/applications/application1/main.js
+++ _tsh/hpw/DependencyInjection-master_/JavaScript/applications/application1/main.js
@@ -5,20 +5,20 @@
 // another pice of code from `framework.js`. Read README.md for tasks.
 
 // Print from the global context of application module
-api.console.log('From application1 global context');
+api.console.log ( 'From application1 global context' );
 
 module.exports = () => {
-  // Print from the exported function context
+    // Print from the exported function context
 
-  api.fs.readFile('../../README.md', (err, data) => {
-    if (err) {
-      api.console.log(err.message);
-      return;
-    }
-    api.console.log(data.toString());
-  });
+    api.fs.readFile ( '../../README.md', ( err, data ) => {
+        if ( err ) {
+            api.console.log ( err.message );
+            return;
+        }
+        api.console.log ( data.toString () );
+    } );
 
-  api.timers.setTimeout(() => {
-    api.console.log('From application1 exported function');
-  }, 5000);
+    api.timers.setTimeout ( () => {
+        api.console.log ( 'From application1 exported function' );
+    }, 5000 );
 };
---
+++ _tsh/hpw/DependencyInjection-master_/JavaScript/sandboxed-fs.js
@@ -0,0 +1,34 @@
+const Fs = require ( 'fs' );
+const Path = require ( 'path' );
+
+const wrapFunction = ( key, fn ) => {
+
+    return ( ...args ) => {
+        debugger
+        const arr_path = args[0].split ( '/' );
+        const wrap_path = Path.join ( './', arr_path[arr_path.length - 1] );
+        args[0] = wrap_path;
+
+        return fn ( ...args );
+    };
+};
+
+const cloneInterface = anInterface => {
+    debugger
+    const clone = {};
+    for ( const key in anInterface ) {
+        const fn = anInterface[key];
+        clone[key] = wrapFunction ( key, fn );
+    }
+    return clone;
+};
+
+const clone_Fs = cloneInterface ( Fs );
+
+// clone_Fs.readFile ( '../../src/README.md', 'utf-8', ( err, data ) => {
+//     debugger
+//     if ( err ) throw err;
+//     console.log ( { data } );
+// } );
+module.exports = clone_Fs;
+
