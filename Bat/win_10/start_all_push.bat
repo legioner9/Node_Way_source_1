b672242a9b26a41bfc1499108835611479038fbc
Legioner9 <legioner9@inbox.ru>
2021-05-06 19:10:49 +0700

    <> edit_3 start_all_push.bat

--- Bat/win_10/start_all_push.bat
+++ Bat/win_10/start_all_push.bat
@@ -1,6 +1,21 @@
 cd F:\Node_projects\Node_Way\Bat\win_10\1_git_gc_push_all.bat
-1_git_gc_push_all_.bat
+git add .
+git commit
+git push disk_tosh master
+git push github_l9 master
+git push usb_32 master&
+pause
 cd F:\Node_projects\AveMetaRu\_bat\win_10\
-push_to_all.bat
+git add .
+git commit
+git push disk_tosh master
+git push github_l9 master
+git push usb_32 master&
+pause
 cd F:\Node_projects\Libellus_03052k21\Bat\win_10\
-1_git_gc_push_all.bat 
+git add .
+git commit
+git push disk_tosh master
+git push github_l9 master
+git push usb_32 master&
+pause
