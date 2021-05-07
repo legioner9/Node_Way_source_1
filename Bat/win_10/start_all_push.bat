cb6ff0fb64c0e3b4dae928c122275b201dab9fba
Legioner9 <legioner9@inbox.ru>
2021-05-07 13:49:27 +0700

    <> edit start_all_push.bat

--- Bat/win_10/start_all_push.bat
+++ Bat/win_10/start_all_push.bat
@@ -1,7 +1,5 @@
 cd F:\Node_projects\Node_Way\Bat\win_10\1_git_gc_push_all.bat
-echo ''
 echo {___Node_Way___}
-pause
 git add .
 git commit
 git push disk_tosh master
@@ -9,8 +7,6 @@ git push github_l9 master
 git push usb_32 master&
 pause
 cd F:\Node_projects\AveMetaRu\_bat\win_10\
-pause
-echo ''
 echo {___AveMetaRu___}
 git add . 
 git commit
@@ -19,8 +15,6 @@ git push github_l9 master
 git push usb_32 master&
 pause
 cd F:\Node_projects\Libellus_03052k21\Bat\win_10\
-pause
-echo ''
 echo {___Libellus_03052k21___}
 git add .
 git commit
