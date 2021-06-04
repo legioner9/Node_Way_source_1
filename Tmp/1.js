e222e95aec6308e4396af2f538927d212f7c2dab
Legioner9 <legioner9@inbox.ru>
2021-06-04 22:59:27 +0700

    <> init node_modules_/_praevia/getPropertyProto/index.js

--- Tmp/1.js
+++ Tmp/1.js
@@ -1,21 +1,16 @@
-const createPromise = (value, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(value);
-        }, delay)
-    })
-}
+debugger
+global.api = {};
+api.fs = require('fs');
+api.http = require('http');
+require('./db');
+require('./application');
+global.db = new api.db.createDatabase();
 
-const p1 = createPromise('Pr_1',200);
-const p2 = createPromise('Pr_1',300);
-const p3 = createPromise('Pr_1',100);
-
-async function start (){
-    const res = await Promise.race([p1,p2,p3]);  
-    res   
-}
-
-start()
+// In DB module: db.js
+api.db = {};
+api.db.createDatabase = function() {
+  console.log('Create DB');
+};
 
 
 
