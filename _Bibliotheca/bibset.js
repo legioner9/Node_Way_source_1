daa4c128eb33b6a65d7a603153f3d6053d23757d
[win]Legioner9 <legioner9@inbox.ru>
2021-08-31 23:57:28 +0700

    <> add _Bibliotheca/

--- _Bibliotheca/bibset.js
+++ _Bibliotheca/bibset.js
@@ -5,5 +5,45 @@ const path = require('path');
 
 const objExport = {};
 
-const arrPath = path.join(__dirname);
-debugger
+const checkIgnore = (arrPath) => {
+  if (~arrPath.indexOf('.bibignore')) {
+    return fs.readFileSync('./.bibignore', 'utf8')
+    .split('\n')
+    .filter(item => item.length || typeof item !== 'string');
+  } else throw new Error('.bibignore file not found');
+};
+
+const filterIgnored = (arrPath, ignoreArray) => {
+  return arrPath.filter(item => {
+    let result = false;
+    ignoreArray.forEach(ignore => {
+
+      const pathToItem = path.join(__dirname, item);
+
+      const fss = fs.statSync(pathToItem);
+      if (!fss.isDirectory()) return result = true;
+
+      const contentDir = fs.readdirSync(pathToItem);
+
+      if (!~contentDir.indexOf('index.js')) return result = true;
+
+      const regex = new RegExp(ignore, 'i');
+      const boolean = Boolean(~item.search(regex));
+      result = result || boolean;
+    });
+    return !result;
+  });
+};
+
+const arrPath = fs.readdirSync(__dirname);
+
+const ignoreArray = checkIgnore(arrPath);
+
+const cleanPath = filterIgnored(arrPath, ignoreArray);
+
+cleanPath.forEach(item => {
+  objExport[item] = require(path.join(__dirname, item, 'index.js'));
+});
+
+module.exports = objExport;
+
